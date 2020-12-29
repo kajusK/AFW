4a175c067c57492e2345fc8a5314cfdd87612884
Jakub Kaderka <jakub.kaderka@gmail.com>
2020-12-29 13:06:06 +0100

    Added DMA support to ADC hal driver

--- sources/hal/adc.c
+++ sources/hal/adc.c
@@ -26,11 +26,8 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/dma.h>
-
+#include <utils/assert.h>
 #include "hal/adc.h"
-
-#define ADC_TEMP_CHANNEL    16
-#define ADC_INT_REF_CHANNEL 17
 
 #define ADC1_DMA_CHANNEL DMA_CHANNEL1
 
@@ -47,13 +44,7 @@
 /** Current vdda voltage (can be measured by adc) */
 static uint16_t adcdi_vdda_mv = 3300;
 
-/**
- * Read raw output from adc measurement
- *
- * @param channel   Channel to read
- * @return Raw adc 12 bit value
- */
-static uint16_t Adcdi_ReadRaw(uint8_t channel)
+uint16_t Adcd_ReadRaw(uint8_t channel)
 {
     adc_set_regular_sequence(ADC1, 1, &channel);
     adc_start_conversion_regular(ADC1);
@@ -64,22 +55,18 @@ static uint16_t Adcdi_ReadRaw(uint8_t channel)
     return adc_read_regular(ADC1);
 }
 
-uint16_t Adcd_ReadMv(uint8_t channel)
+uint16_t Adcd_RawToMv(uint16_t raw)
 {
-    uint16_t raw = Adcdi_ReadRaw(channel);
-
     return (uint32_t) adcdi_vdda_mv * raw / ADC_MAX;
 }
 
-uint16_t Adcd_ReadVccMv(void)
+uint16_t Adcd_RawToVcc(uint16_t raw)
 {
-    uint16_t raw = Adcdi_ReadRaw(ADC_INT_REF_CHANNEL);
     return 3300U * VREFINT_CAL/raw;
 }
 
-int8_t Adcd_ReadTempDegC(void)
+int8_t Adcd_RawToTemp(uint16_t raw)
 {
-    uint16_t raw = Adcdi_ReadRaw(ADC_TEMP_CHANNEL);
     uint16_t ref_mv = adcdi_vdda_mv;
     int32_t temp;
 
@@ -88,9 +75,9 @@ int8_t Adcd_ReadTempDegC(void)
     return temp;
 }
 
-void Adcd_UpdateVdda(void)
+void Adcd_UpdateVddaRaw(uint16_t raw)
 {
-    adcdi_vdda_mv = Adcd_ReadVccMv();
+    adcdi_vdda_mv = Adcd_RawToVcc(raw);
 }
 
 void Adcd_Sleep(void)
@@ -105,12 +92,46 @@ void Adcd_Wakeup(void)
     adc_power_on(ADC1);
     adc_enable_temperature_sensor();
     adc_enable_vrefint();
+}
 
-    /* Wait for ADC starting up. */
-    for (int i = 0; i < 800000; i++) {
-        __asm__("nop");
-    }
-    Adcd_UpdateVdda();
+bool Adcd_DmaDataValid(void)
+{
+    bool ret = dma_get_interrupt_flag(DMA1, ADC1_DMA_CHANNEL, DMA_TCIF);
+    dma_clear_interrupt_flags(DMA1, ADC1_DMA_CHANNEL, DMA_TCIF);
+    return ret;
+}
+
+void Adcd_InitDma(uint16_t *data, const uint8_t *channels, uint8_t length)
+{
+    ASSERT_NOT(data == NULL || channels == NULL || length < 1 || length > 18);
+
+    Adcd_Init();
+    adc_power_off(ADC1);
+    adc_set_operation_mode(ADC1, ADC_MODE_SCAN_INFINITE);
+    adc_set_regular_sequence(ADC1, length, (uint8_t *) channels);
+
+    rcc_periph_clock_enable(RCC_DMA1);
+    dma_channel_reset(DMA1, ADC1_DMA_CHANNEL);
+    dma_set_priority(DMA1, ADC1_DMA_CHANNEL, DMA_CCR_PL_LOW);
+    dma_set_number_of_data(DMA1, ADC1_DMA_CHANNEL, length);
+
+    dma_set_peripheral_address(DMA1, ADC1_DMA_CHANNEL, (uint32_t)&ADC_DR(ADC1));
+    dma_set_peripheral_size(DMA1, ADC1_DMA_CHANNEL, DMA_CCR_PSIZE_16BIT);
+    dma_disable_peripheral_increment_mode(DMA1, ADC1_DMA_CHANNEL);
+    dma_set_read_from_peripheral(DMA1, ADC1_DMA_CHANNEL);
+
+    dma_set_memory_address(DMA1, ADC1_DMA_CHANNEL, (uint32_t) data);
+    dma_set_memory_size(DMA1, ADC1_DMA_CHANNEL, DMA_CCR_MSIZE_16BIT);
+    dma_enable_memory_increment_mode(DMA1, ADC1_DMA_CHANNEL);
+
+    dma_enable_channel(DMA1, ADC1_DMA_CHANNEL);
+    dma_enable_circular_mode(DMA1, ADC1_DMA_CHANNEL);
+    adc_enable_dma_circular_mode(ADC1);
+    adc_enable_dma(ADC1);
+
+    adc_power_on(ADC1);
+    dma_clear_interrupt_flags(DMA1, ADC1_DMA_CHANNEL, DMA_TCIF);
+    adc_start_conversion_regular(ADC1);
 }
 
 void Adcd_Init(void)
@@ -119,7 +140,6 @@ void Adcd_Init(void)
 
     adc_power_off(ADC1);
     adc_calibrate(ADC1);
-    adc_power_on(ADC1);
 
     adc_set_clk_source(ADC1, ADC_CLKSOURCE_PCLK_DIV4);
     adc_set_sample_time_on_all_channels(ADC1, ADC_SMPTIME_071DOT5);
@@ -130,13 +150,7 @@ void Adcd_Init(void)
     adc_disable_analog_watchdog(ADC1);
     adc_enable_temperature_sensor();
     adc_enable_vrefint();
-
-    /* Wait for ADC starting up. */
-    for (int i = 0; i < 800000; i++) {
-        __asm__("nop");
-    }
-
-    Adcd_UpdateVdda();
+    adc_power_on(ADC1);
 }
 
 /** @} */
--- sources/hal/adc.h
+++ sources/hal/adc.h
@@ -28,34 +28,82 @@
 
 #include <types.h>
 
+/** Channel with thermometer connected */
+#define ADC_TEMP_CHANNEL    16
+/** Channel with internal voltage reference (for measuring Vcc) */
+#define ADC_INT_REF_CHANNEL 17
+
+/**
+ * Measure channel and get a raw ADC output value
+ *
+ * Do not use while using a DMA mode. The call is blocking.
+ *
+ * @param channel   Channel to read
+ * @return Raw ADC 12 bit value
+ */
+uint16_t Adcd_ReadRaw(uint8_t channel);
+
+/**
+ * Convert raw value to mV
+ *
+ * @param raw       Raw data measured by ADC
+ * @return Converted voltage in mV
+ */
+extern uint16_t Adcd_RawToMv(uint16_t raw);
+
+/**
+ * Convert raw value from internal reference to Vcc in mV
+ *
+ * @param raw       Raw data from the internal reference measurement
+ * @return Converted MCU supply voltage in mV
+ */
+extern uint16_t Adcd_RawToVcc(uint16_t raw);
+
+/**
+ * Convert raw value from the internal temperature sensor to degrees C
+ *
+ * @param raw       Raw data from the internal temperature sensor
+ * @return Temperature in degrees C
+ */
+extern int8_t Adcd_RawToTemp(uint16_t raw);
+
+/**
+ * Update the Vdda used in calculations to value measured from internal reference
+ *
+ * Should be called from time to time if vdda is not very stable
+ *
+ * @param raw       Raw data from the internal reference measurement
+ */
+extern void Adcd_UpdateVddaRaw(uint16_t raw);
+
 /**
  * Read voltage on the channel in mV
  *
  * @param channel       Channel to read data from
  * @return voltage in mV
  */
-extern uint16_t Adcd_ReadMv(uint8_t channel);
+#define Adcd_ReadMv(channel) Adcd_RawToMv(Adcd_ReadRaw(channel))
 
 /**
  * Read power supply voltage
  *
  * @return Vdda voltage in mv
  */
-extern uint16_t Adcd_ReadVccMv(void);
+#define Adcd_ReadVccMv() Adcd_RawToVcc(Adcdi_ReadRaw(ADC_INT_REF_CHANNEL))
 
 /**
  * Read core temeperature
  *
  * @return temperature in degreec C
  */
-extern int8_t Adcd_ReadTempDegC(void);
+#define Adcd_ReadTempDegC() Adcd_RawToTemp(Adcd_ReadRaw(ADC_TEMP_CHANNEL))
 
 /**
  * Update reference voltage from internal reference measurements
  *
  * Should be called from time to time if vdda is not very stable
  */
-extern void Adcd_UpdateVdda(void);
+#define Adcd_UpdateVdda() Adcd_UpdateVddaRaw(Adcd_ReadRaw(ADC_INT_REF_CHANNEL))
 
 /**
  * Put adc peripheral to low power mode
@@ -64,11 +112,42 @@ extern void Adcd_Sleep(void);
 
 /**
  * Put adc peripheral to normal mode
+ *
+ * It takes a while for ADC to start, take first measurement after few dozens
+ * of ms. Additionally, call Adcd_UpdateVdda to measure a real supply voltage
+ * else the Adcd_ReadMv will assume the Vdda is 3,3 V
  */
 extern void Adcd_Wakeup(void);
 
 /**
+ * Check if the data stored by DMA are valid (first measurement finished)
+ *
+ * The valid flag in the DMA is cleared, will be set after whole group is
+ * measured again
+ *
+ * @return true if data valid
+ */
+extern bool Adcd_DmaDataValid(void);
+
+/**
+ * Initialize ADC in DMA mode
+ *
+ * When in DMA mode, no direct measuring functions should be called, use
+ * raw variants instead to convert the measured value to something meaningful.
+ *
+ * The data are measured automatically in a loop and stored by DMA to memory
+ * @param data      Memory area to store DMA results to
+ * @param channels  Channels to measure - on F0 the channels must be sorted (1 to 18)
+ * @param length    Amount of channels to measure
+ */
+extern void Adcd_InitDma(uint16_t *data, const uint8_t *channels, uint8_t length);
+
+/**
  * Initialize the adc driver in single measurement mode
+ *
+ * It takes a while for ADC to start, take first measurement after few dozens
+ * of ms. Additionally, call Adcd_UpdateVdda to measure a real supply voltage
+ * else the Adcd_ReadMv will assume the Vdda is 3,3 V
  */
 extern void Adcd_Init(void);
 
